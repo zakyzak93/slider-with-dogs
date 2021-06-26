bd2938bd1e62129b0f6459b8cd8d8061a074db8d
zakyzak93 <degtyarev.yurij@mail.ru>
2021-06-26 19:58:22 +0300

    add function slidesPlugin

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const slides = document.querySelectorAll('.slide');
 
-slides.forEach((slide) =>{
-    slide.addEventListener('click', ()=>{
-        clearActiveClasses();
-        slide.classList.add('active');
-        player.loadVideoById({'videoId': slide.dataset.videoId,
-               'startSeconds': startSeconds,
-               'endSeconds': endSeconds,});
+function slidesPlugin (activeSlide) {
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active')
+    
+    slides.forEach((slide) =>{
+        slide.addEventListener('click', ()=>{
+            clearActiveClasses();
+            slide.classList.add('active');
+            player.loadVideoById({'videoId': slide.dataset.videoId,
+                   'startSeconds': startSeconds,
+                   'endSeconds': endSeconds,});
+        });
     });
-});
-function clearActiveClasses(){
-    slides.forEach((slide) => slide.classList.remove('active'))
-};
+    function clearActiveClasses(){
+        slides.forEach((slide) => slide.classList.remove('active')
+        )
+    }
+}
 
+slidesPlugin (2)
